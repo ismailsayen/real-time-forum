5453cdc5e40c702affce576c55b222d36d9bcd40
fahdaguenouz <faguenouz@gmail.com>
2025-04-17 23:15:28 +0100

    adding error sender and checking the fields]

--- api/handlers/auth/register.go
+++ api/handlers/auth/register.go
@@ -10,32 +10,33 @@ import (
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		data := &database.Error{Message: "Methode Not Allowed", Status: http.StatusMethodNotAllowed}
-		resp, err := utils.Marshal(w, data, data.Status)
-		if err != nil {
-			http.Error(w, "Server error", http.StatusInternalServerError)
-			return
-		}
-		w.Write(resp)
+		utils.SendError(w, http.StatusMethodNotAllowed, "Method Not Allowed")
 		return
 	}
 	var user database.User
 
 	if r.Body == nil {
-		http.Error(w, "Empty request body", http.StatusBadRequest)
+		utils.SendError(w, http.StatusBadRequest, "Empty request body")
 		return
 	}
-		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-			http.Error(w, "Invalid json format data", http.StatusBadRequest)
-			return
-		}
-	if  user.Email == "" || user.FirstName == "" || user.Gender == "" || user.LastName == "" || user.Password == "" || user.Username == ""||user.Age == 0 {
-    // fmt.Println("error less data ")
-	http.Error(w, "All fields are required", http.StatusBadRequest)
-	return 
-}
-fmt.Println("succ",user)
-	
-	
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		utils.SendError(w, http.StatusBadRequest, "Invalid json format data")
+		return
+	}
+	if user.Email == "" || user.FirstName == "" || user.Gender == "" || user.LastName == "" || user.Password == "" || user.Username == "" || user.Age == 0 {
+		// fmt.Println("error less data ")
+		utils.SendError(w, http.StatusBadRequest, "All fields are required")
+		return
+	}
+	if user.Age <= 18 {
+		utils.SendError(w, http.StatusBadRequest, "Age must be greater than 18")
+		return
+	}
+	if !utils.IsValidEmail(user.Email) {
+		utils.SendError(w, http.StatusBadRequest, "Invalid email format")
+		return
+	}
+
+	fmt.Println("succ", user)
 
 }
