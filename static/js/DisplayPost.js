f5b1cfa74188235c913410684ae106ae8b7b753d
Fahd Aguenouz <faguenouz@gmail.com>
2025-04-20 17:35:19 +0100

    testing login

--- static/js/DisplayPost.js
+++ static/js/DisplayPost.js
@@ -1,4 +1,26 @@
-export function DisplayPost(){
-    const container=document.querySelector(".container")
-    container.innerHTML="post "
+export async function DisplayPost(){
+
+    try {
+        const resp = await fetch("/Getposts");
+        const data = await resp.json();
+    
+        const container = document.querySelector(".container");
+    console.log(data);
+    
+        data.forEach(post => {
+          const postss = document.createElement("div");
+          postss.innerHTML = `
+            <h2>${post.title}</h2>
+            <p><strong>By:</strong> ${post.nickname}</p>
+            <p>${post.content}</p>
+            <p><strong>Categories:</strong> ${post.categories.join(", ")}</p>
+            <hr/>
+          `;
+          container.appendChild(postss);
+        });
+        window.history.pushState(null,'',"/posts")
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    
 }
