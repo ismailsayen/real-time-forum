6dd8d81023c9976f97dff7856d34440634d93dae
Fahd Aguenouz <faguenouz@gmail.com>
2025-04-24 11:15:53 +0100

    adding return

--- api/handlers/commentHandler.go
+++ api/handlers/commentHandler.go
@@ -15,16 +15,19 @@ import (
 func CommentHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method != http.MethodPost {
 		utils.SendError(w, http.StatusMethodNotAllowed, "Metohde Not allowed.")
+		return 
 	}
 	var comment database.Comment
 	json.NewDecoder(r.Body).Decode(&comment)
 	if comment.Content == "" {
 		utils.SendError(w, http.StatusBadRequest, "Cannot send empty comment.")
+		return 
 	}
 	comment.Content = html.EscapeString(strings.TrimSpace(comment.Content))
 
 	err := models.Comment(comment.IdPost, comment.IdUser, comment.Date, comment.Content, db)
 	if err != nil {
 		utils.SendError(w, http.StatusInternalServerError, err.Error())
+		return 
 	}
 }
